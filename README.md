c07ca5b9fdb278bec3b2b6b1050595116a5ec7ec
Gregsen <gkneitschel@gmail.com>
2015-10-26 14:38:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Config Tester
+Tests different methods for loading configurations from application.yml. One should fail, but doesn't.
---
+++ src/main/java/de/gregsen/MainApp.java
@@ -0,0 +1,15 @@
+package de.gregsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(Configuration.class)
+public class MainApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+}
